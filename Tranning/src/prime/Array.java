f1b84dd5f786f98526b2ef879732a3ff3433944c
Dheeraj <Dheeraj@SHALINI>
2023-10-19 22:24:23 +0530

    first commit

--- Tranning/src/prime/Array.java
+++ Tranning/src/prime/Array.java
@@ -9,6 +9,7 @@ public class Array {
 		 a[2]=3;
 		 a[3]=6;
 		 a[4]=8;
+		 System.out.println(a[3]);//for displaying one array index
 		for(int  i=0;i<a.length;i++)
 		{
 			System.out.println(a[i]);
